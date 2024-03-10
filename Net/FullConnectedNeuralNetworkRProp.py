b01de5a09924e6cf8d4842e9e914fb5b12319cdc
MarianoCaccavale <75137536+MarianoCaccavale@users.noreply.github.com>
2024-03-10 13:59:52 +0100

    Vectorized _update_step_size, small code refactor

--- Net/FullConnectedNeuralNetworkRProp.py
+++ Net/FullConnectedNeuralNetworkRProp.py
@@ -1,17 +1,17 @@
 # Utilizzo la Resilient BackPropagation di tipo "without weight-backtracking"
 from typing import List, Callable
 import numpy as np
-from Layer import *
+from Net.Layer import *
 from Net.FullConnectedNeuralNetwork import FullConnectedNeuralNetwork
 
 
 class FullConnectedNeuralNetworkRprop(FullConnectedNeuralNetwork):
 
-  def __init__(self, layers: List[Layer], cost_function: Callable[[np.array, np.array], float ], learning_rate: int = 0.01,
-               rprop_eta_plus: float = 1.5, rprop_eta_minus: float = 0.5,
-               max_step_size: float = 10., min_step_size:float = 0.):
+  def __init__(self, layers: List[Layer], cost_function: Callable[[np.array, np.array], float ],
+               rprop_eta_plus: float = 1.2, rprop_eta_minus: float = 0.5,
+               max_step_size: float = 50., min_step_size:float = 1e-6):
 
-    super().__init__(layers, cost_function, learning_rate)
+    super().__init__(layers, cost_function, 1)
 
     if (rprop_eta_minus >= 1 or rprop_eta_minus <= 0):
       raise Exception("Il parametro eta- della Resilient Backpropagation deve essere compreso tra ]0, 1[")
@@ -32,27 +32,28 @@ class FullConnectedNeuralNetworkRprop(FullConnectedNeuralNetwork):
     self.step_sizes = []
 
     for layer in self.layers:
-      self.step_sizes.append(np.zeros(layer.weights.shape))
-      # self.step_sizes.append(np.random.uniform(low = self.min_step_size, high = self.max_step_size, size = layer.weights.shape))
+      self.step_sizes.append(np.full(layer.weights.shape, self.max_step_size/2))
 
   def _compute_gradient_change(self, last_gradient: np.array, current_gradient: np.array):
     return np.multiply(np.sign(last_gradient), np.sign(current_gradient))
 
   def _update_step_size(self, layer_index: int, gradient_change: np.ndarray):
-    for row_index in range(0, gradient_change.shape[0]):
-      for col_index in range(0, gradient_change.shape[1]):
-        if gradient_change[row_index][col_index] > 0:
-          self.step_sizes[layer_index][row_index][col_index] = min(self.step_sizes[layer_index][row_index][col_index] * self.rprop_eta_plus, self.max_step_size)
-        elif gradient_change[row_index][col_index] < 0:
-          self.step_sizes[layer_index][row_index][col_index] = max(self.step_sizes[layer_index][row_index][col_index] * self.rprop_eta_minus, self.min_step_size)
 
-  def _improve_gradient(self, gradient_diffs: np.array, current_gradient: np.array):
-    for row_index in range(0, gradient_diffs.shape[0]):
-      for col_index in range(0, gradient_diffs.shape[1]):
-        if gradient_diffs is not None and gradient_diffs[row_index, col_index] < 0:
-          current_gradient[row_index][col_index] = 0
+    self.step_sizes[layer_index] = np.where(gradient_change > 0, 
+                                            np.minimum(self.step_sizes[layer_index] * self.rprop_eta_plus, self.max_step_size), 
+                                            self.step_sizes[layer_index])
+  
+    self.step_sizes[layer_index] = np.where(gradient_change < 0, 
+                                            np.maximum(self.step_sizes[layer_index] * self.rprop_eta_minus, self.min_step_size), 
+                                            self.step_sizes[layer_index])
 
-    return current_gradient
+    
+  # def _improve_gradient(self, gradient_diffs: np.array, current_gradient: np.array):
+  #   for row_index in range(0, gradient_diffs.shape[0]):
+  #     for col_index in range(0, gradient_diffs.shape[1]):
+  #       if gradient_diffs is not None and gradient_diffs[row_index, col_index] < 0:
+  #         current_gradient[row_index][col_index] = 0
+  #   return current_gradient
 
   def _back_propagate(self, ground_truth: np.array):
 
@@ -64,51 +65,38 @@ class FullConnectedNeuralNetworkRprop(FullConnectedNeuralNetwork):
 
     gradient_change = None
 
-    for layer_index in reversed(range(0, len(self.layers))):
+    for layer_index in range(len(self.layers)-1, -1, -1):
 
       layer = self.layers[layer_index]
 
       # Output layer
       if layer_index == len(self.layers) - 1:
-        dCdA = self.cost_function.compute_derivate(layer.A, ground_truth)
-        # Faccio diventare il vettore colonna dZ una matrice con una sola colonna, che è proprio il vettore colonna. Questo per far quadrare le dimensioni nelle moltiplicazioni
-        # successive
-        dCdA = dCdA[:, np.newaxis]
+        dCdA = self.cost_function.compute_derivate(layer.A, ground_truth)[:, np.newaxis]
         dZdW = self.layers[layer_index - 1].A[:, np.newaxis].T
-        dCdW = (1/layer.number_of_neurons) * np.dot(dCdA, dZdW)
-
-        # Se invece ho già salvato la derivata della funzione di costo aggiorno gli step moltiplicando l'attuale costo per il costo del "tempo" precedente
-        if self.last_gradient_per_layer[layer_index] is not None:
-          gradient_change = self._compute_gradient_change(self.last_gradient_per_layer[layer_index], dCdW)
-          self._update_step_size(layer_index, gradient_change)
+        dCdW = np.dot(dCdA, dZdW) / layer.number_of_neurons
 
       else:
-        # N.B. dCdA[i]       = w[i+1].T x dZ[i+1] * g'[i](Z[i])
-        #      dCdA[i]       = first_part         * dAdZ
-        #      first_part    = np.dot(self.layers[layer_index + 1].weights.T, last_dCdA)
-        #      dAdZ          = compute_derivate(layer.Z)
-
         dAdZ = layer.activation_function_derivative[:, np.newaxis]
-
         first_part = np.dot(self.layers[layer_index + 1].weights.T, last_dCdA)
         dCdA = first_part *  dAdZ
-        dCdW = (1/layer.number_of_neurons) * np.dot(dCdA, layer.X[:, np.newaxis].T)
+        dCdW = np.dot(dCdA, layer.X[:, np.newaxis].T) / layer.number_of_neurons
 
-        # Se invece ho già salvato la derivata della funzione di costo aggiorno gli step moltiplicando l'attuale costo per il costo del "tempo" precedente
-        if self.last_gradient_per_layer[layer_index] is not None:
-          gradient_change = self._compute_gradient_change(self.last_gradient_per_layer[layer_index], dCdW)
-          self._update_step_size(layer_index, gradient_change)
+      # Se invece ho già salvato la derivata della funzione di costo aggiorno gli step moltiplicando l'attuale costo 
+      # per il costo del "tempo" precedente
+      if self.last_gradient_per_layer[layer_index] is not None:
+        gradient_change = self._compute_gradient_change(self.last_gradient_per_layer[layer_index], dCdW)
+        self._update_step_size(layer_index, gradient_change)
 
       dw = np.multiply(-np.sign(dCdW), self.step_sizes[layer_index])
 
-      if gradient_change is not None:
-        dCdW = self._improve_gradient(gradient_change, dCdW)
+      #if gradient_change is not None:
+      #  dCdW = self._improve_gradient(gradient_change, dCdW)
 
-      db = (1/layer.number_of_neurons) * np.sum(dCdA, axis = 1, keepdims=True)
+      db = np.sum(dCdA, axis = 1, keepdims=True) / layer.number_of_neurons
 
       self.last_gradient_per_layer[layer_index] = dCdW
 
-      weights_delta.append(-dw)
+      weights_delta.append(dw)
       biases_delta.append(db)
 
       last_dCdA = dCdA
