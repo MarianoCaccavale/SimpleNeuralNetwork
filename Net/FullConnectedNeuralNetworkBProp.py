6c7decb73bd27966924cb23e12e399194d76736b
MarianoCaccavale <75137536+MarianoCaccavale@users.noreply.github.com>
2024-03-10 19:08:27 +0100

    THE BPROP WORKS AGAIN.

--- Net/FullConnectedNeuralNetworkBProp.py
+++ Net/FullConnectedNeuralNetworkBProp.py
@@ -3,7 +3,7 @@ from Net.FullConnectedNeuralNetwork import *
 class FullConnectedNeuralNetworkBprop(FullConnectedNeuralNetwork):
   def _back_propagate(self, ground_truth: np.array):
 
-    last_dCdA = 0
+    last_delta_valore = 0
     layer_index = 0
 
     weights_delta = []
@@ -12,27 +12,25 @@ class FullConnectedNeuralNetworkBprop(FullConnectedNeuralNetwork):
     for layer_index in range(len(self.layers)-1, -1, -1):
 
       layer = self.layers[layer_index]
+      
+      dAdZ = layer.activation_function_derivative[:, np.newaxis] # 10, 1
+      dZdW = self.layers[layer_index - 1].A[:, np.newaxis] if layer_index != 0 else self.layers[layer_index].X[:, np.newaxis] # 256, 1
 
       # Output layer
       if layer_index == len(self.layers) - 1:
-        dCdA = self.cost_function.compute_derivate(layer.A, ground_truth)[:, np.newaxis]
-        dCdA *= layer.activation_function_derivative
-        dZdW = self.layers[layer_index - 1].A[:, np.newaxis].T
-        
+        dCdA = self.cost_function.compute_derivate(layer.A, ground_truth)[:, np.newaxis] # 10, 1
       else:
-        dAdZ = layer.activation_function_derivative[:, np.newaxis]
-        first_part = np.dot(self.layers[layer_index + 1].weights.T, last_dCdA)
-        dCdA = first_part * dAdZ
-        dZdW = layer.X[:, np.newaxis].T
+        dCdA = np.dot(self.layers[layer_index + 1].weights.T, last_delta_valore)
 
-      dw = np.dot(dCdA, dZdW) / layer.number_of_neurons
-        
-      db = np.sum(dCdA, axis = 1, keepdims=True) / layer.number_of_neurons
+      delta_valore = np.multiply(dAdZ, dCdA) #10, 1
+      dw = np.dot(delta_valore, dZdW.T)# / layer.number_of_neurons
+      
+      db = np.sum(dCdA, axis = 1, keepdims=True)# / layer.number_of_neurons
 
-      weights_delta.append(-dw)
+      weights_delta.append(dw)
       biases_delta.append(db)
 
-      last_dCdA = dCdA
+      last_delta_valore = delta_valore
 
     # I delta dei pesi e i delta dei biases sono "al contrario", ovvero quelli in posizione 0 della lista in realtà sono per il layer di output, quindi faccio il
     # reverse della lista per averli sistemati
