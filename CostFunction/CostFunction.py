6848e7fc0233bbcdcbcb22e56f1811082990744c
MarianoCaccavale <75137536+MarianoCaccavale@users.noreply.github.com>
2024-03-10 13:22:32 +0100

    Improved Cost function code

--- CostFunction/CostFunction.py
+++ CostFunction/CostFunction.py
@@ -3,28 +3,28 @@ from abc import abstractmethod
 
 class CostFunction():
   @abstractmethod
-  def compute(output: np.array, ground_truth: np.array) -> float:
+  def compute(output: np.ndarray, ground_truth: np.ndarray) -> float:
     raise NotImplementedError("Method not implemented")
   @abstractmethod
-  def compute_derivate(output: np.array, ground_truth: np.array) -> float:
+  def compute_derivate(output: np.ndarray, ground_truth: np.ndarray) -> float:
     raise NotImplementedError("Method not implemented")
 
 class SumOfSquaredError(CostFunction):
-  def compute(output: np.array, ground_truth: np.array) -> float:
+  def compute(output: np.ndarray, ground_truth: np.ndarray) -> float:
     return 0.5 * np.sum(np.square((output - ground_truth)))
-  def compute_derivate(output: np.array, ground_truth: np.array) -> float:
+  def compute_derivate(output: np.ndarray, ground_truth: np.ndarray) -> float:
     return output - ground_truth
 
 class MeanSquaredError(CostFunction):
-  def compute(output: np.array, ground_truth: np.array) -> float:
+  def compute(output: np.ndarray, ground_truth: np.ndarray) -> float:
     return (1 / output.shape[0]) * np.sum(np.square((output - ground_truth)))
-  def compute_derivate(output: np.array, ground_truth: np.array) -> float:
+  def compute_derivate(output: np.ndarray, ground_truth: np.ndarray) -> float:
     return (2 / output.shape[0]) * np.sum((output - ground_truth))
 
 class CrossEntropy(CostFunction):
-  def compute(output: np.array, ground_truth: np.array) -> float:
-    observation_log = np.log(output)
+  def compute(output: np.ndarray, ground_truth: np.ndarray) -> float:
+    observation_log = np.log(output, where=output!=0)
     log_losses = -np.multiply(ground_truth, observation_log)
     return np.sum(log_losses)
-  def compute_derivate(output: np.array, ground_truth: np.array) -> float:
+  def compute_derivate(output: np.ndarray, ground_truth: np.ndarray) -> float:
     return output - ground_truth
