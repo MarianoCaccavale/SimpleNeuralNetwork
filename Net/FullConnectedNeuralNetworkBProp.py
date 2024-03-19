041e9c5ac4a3743c12865c40b113ac9b5af8517b
MarianoCaccavale <75137536+MarianoCaccavale@users.noreply.github.com>
2024-03-19 13:39:38 +0100

    End refactor in BPROP class

--- Net/FullConnectedNeuralNetworkBProp.py
+++ Net/FullConnectedNeuralNetworkBProp.py
@@ -2,7 +2,7 @@ from numpy import ndarray
 from Net.FullConnectedNeuralNetwork import *
 
 class FullConnectedNeuralNetworkBprop(FullConnectedNeuralNetwork):
-  def _update_parameters(self, dw: List[ndarray], db: List[ndarray]):
+  def _update_parameters(self, dw: List[np.ndarray], db: List[np.ndarray]):
     for layer_index in range(len(self.layers)):
       delta_weight = -(dw[layer_index] * self.learning_rate)
       delta_bias = -(db[layer_index] * self.learning_rate)
